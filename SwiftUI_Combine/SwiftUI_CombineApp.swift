7307cf6ca8698d3b862c9062c8a329ac265b82ab
sudoswift <sudoswift@gmail.com>
2021-06-02 02:41:12 +0900

    Initial Commit

---
+++ SwiftUI_Combine/SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CombineApp.swift
+//  SwiftUI_Combine
+//
+//  Created by izvern on 2021/06/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
